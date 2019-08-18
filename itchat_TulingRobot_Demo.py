90bf6bd89d6c2e4edc97d339504f12954ee2e2ae
htw01118891 <huangtianwei1988@163.com>
2019-08-18 20:34:49 +0800

    创建文件-201908181549

--- itchat_TulingRobot_Demo.py
+++ itchat_TulingRobot_Demo.py
@@ -1,3 +1,4 @@
+"""
 import requests
 apiul="http://www.tuling123.com/openapi/api"
 #data={"key":"3b04f75866084b30bede0d341c98c3db","info":"我是谁?","userId":"TulingRobot"}
@@ -6,3 +7,19 @@ r=requests.post(apiul,data=data).json()
 print(type(r))
 print(r)
 print(r["text"])
+"""
+import requests
+#cityID:101280601-深圳
+r_Shenzhen = requests.get('http://www.weather.com.cn/data/sk/101280601.html')
+#cityID:101230201-厦门
+r_Xiamen = requests.get('http://www.weather.com.cn/data/sk/101230201.html')
+#print(type(r))
+#print(r)
+r_Shenzhen.encoding = 'utf-8'
+r_Xiamen.encoding = 'utf-8'
+#print(type(r.json()))
+print(r_Shenzhen.json())
+print(r_Xiamen.json())
+print(r_Shenzhen.content)
+print(r_Xiamen.content)
+
