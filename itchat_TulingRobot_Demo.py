7f482d3fb1a684fc9119ef617fb6d0d61b9f5ba1
htw01118891 <huangtianwei1988@163.com>
2019-08-18 19:17:15 +0800

    创建文件-201908181549

--- itchat_TulingRobot_Demo.py
+++ itchat_TulingRobot_Demo.py
@@ -3,4 +3,6 @@ apiul="http://www.tuling123.com/openapi/api"
 #data={"key":"3b04f75866084b30bede0d341c98c3db","info":"我是谁?","userId":"TulingRobot"}
 data={"key":"024848ea750a46f993eba7975330dd1d","info":"你是谁?","userId":"TulingRobot"}
 r=requests.post(apiul,data=data).json()
+print(type(r))
+print(r)
 print(r["text"])
