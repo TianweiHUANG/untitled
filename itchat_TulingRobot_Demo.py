516b16ae6d15f3e7d348992d628969e01580ea65
htw01118891 <huangtianwei1988@163.com>
2019-08-21 12:59:35 +0800

    创建文件-201908181549

--- itchat_TulingRobot_Demo.py
+++ itchat_TulingRobot_Demo.py
@@ -1,13 +1,14 @@
 """user:huangtianwei1988@163.com password:huangtianwei1988@163.com's password"""
-"""
+
 import requests
 apiul="http://www.tuling123.com/openapi/api"
-#data={"key":"3b04f75866084b30bede0d341c98c3db","info":"我是谁?","userId":"TulingRobot"}
-data={"key":"024848ea750a46f993eba7975330dd1d","info":"你是谁?","userId":"TulingRobot"}
+data={"key":"3b04f75866084b30bede0d341c98c3db","info":"我是谁?","userId":"TulingRobot"}
+#data={"key":"024848ea750a46f993eba7975330dd1d","info":"你是谁?","userId":"TulingRobot"}
 r=requests.post(apiul,data=data).json()
 print(type(r))
 print(r)
 print(r["text"])
+
 """
 import requests
 #cityID:101280601-深圳
@@ -16,13 +17,10 @@ r_Shenzhen = requests.get('http://www.weather.com.cn/data/cityinfo/101280601.htm
 #cityID:101230201-厦门
 #r_Xiamen = requests.get('http://www.weather.com.cn/data/sk/101230201.html')
 r_Xiamen = requests.get('http://www.weather.com.cn/data/cityinfo/101230201.html')
-#print(type(r))
-#print(r)
 r_Shenzhen.encoding = 'utf-8'
 r_Xiamen.encoding = 'utf-8'
-#print(type(r.json()))
-print(r_Shenzhen.json())
-print(r_Xiamen.json())
 print(r_Shenzhen.content)
 print(r_Xiamen.content)
-
+print(r_Shenzhen.json())
+print(r_Xiamen.json())
+"""
