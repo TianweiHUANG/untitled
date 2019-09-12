3750c7ae1e848449fc9c5c86bb240792535666bd
htw01118891 <huangtianwei1988@163.com>
2019-09-12 15:47:02 +0800

    创建文件-201908181549

--- TCP_Socket_tool.py
+++ TCP_Socket_tool.py
@@ -1,22 +1,46 @@
 import time
 import globalvar
+import socket
 
 def TCP_Socket_tool():
     while True:
+        #1.创建套接字socket
+        #tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #2.创建套接字socket,连接服务器
         if globalvar.get_value("Connect")==True:
+            tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            dest_ip=globalvar.get_value("Server_IP")
+            dest_port=int(globalvar.get_value("Server_PORT"))
+            dest_addr=(dest_ip,dest_port)
+            tcp_socket.connect(dest_addr)
+
             print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("send_Message"))
-            print("Connect=True")
+                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Connect=True")
             globalvar.set_value("Connect", False)
+
+        #3.接收/发送数据
         if globalvar.get_value("Send")==True:
+            send_data=globalvar.get_value("send_Message")
+            tcp_socket.send(send_data.encode("utf-8"))
+
             print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("send_Message"))
-            print("Send=True")
+                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Send=True")
             globalvar.set_value("Send", False)
-        if globalvar.get_value("Close")==True:
+        if globalvar.get_value("Receive")==True:
+            recv_data = tcp_socket.recv(1024)
+            globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
+
             print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("send_Message"))
-            print("Close=True")
+                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Receive=True")
+            globalvar.set_value("Receive", False)
+
+        #4.关闭套接字socket
+        if globalvar.get_value("Close")==True:
+            tcp_socket.close()
+
+            print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Close=True")
             globalvar.set_value("Close", False)
+
         time.sleep(0.1)
 
--- main.py
+++ main.py
@@ -14,9 +14,12 @@ class MyDialog(QDialog,Ui_Dialog):
         self.lineEdit.textChanged.connect(self.lineEdit_Func)
         self.lineEdit_2.textChanged.connect(self.lineEdit_2_Func)
         self.lineEdit_3.textChanged.connect(self.lineEdit_3_Func)
+
         self.pushButton.clicked.connect(self.pushButton_Func)
         self.pushButton_2.clicked.connect(self.pushButton_2_Func)
         self.pushButton_3.clicked.connect(self.pushButton_3_Func)
+        self.pushButton_4.clicked.connect(self.pushButton_4_Func)
+        self.pushButton_5.clicked.connect(self.pushButton_5_Func)
 
     def lineEdit_Func(self,lineEdit_textChanged):
         globalvar.set_value("Server_IP", lineEdit_textChanged)
@@ -24,12 +27,18 @@ class MyDialog(QDialog,Ui_Dialog):
         globalvar.set_value("Server_PORT", lineEdit_2_textChanged)
     def lineEdit_3_Func(self,lineEdit_3_textChanged):
         globalvar.set_value("send_Message", lineEdit_3_textChanged)
+
     def pushButton_Func(self):
         globalvar.set_value("Connect",True)
     def pushButton_2_Func(self):
         globalvar.set_value("Send",True)
     def pushButton_3_Func(self):
         globalvar.set_value("Close", True)
+    def pushButton_4_Func(self):
+        globalvar.set_value("Receive", True)
+    def pushButton_5_Func(self):
+        self.lineEdit_4.setText(globalvar.get_value("Receive_Message"))
+
 if __name__ == '__main__':
   globalvar._init()
 
