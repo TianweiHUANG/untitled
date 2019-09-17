dceec8c546c3bda5f485f4fd0e39de36acd1f848
htw01118891 <huangtianwei1988@163.com>
2019-09-17 21:06:43 +0800

    创建文件-201908181549

--- TCP_Socket_Receive.py
+++ TCP_Socket_Receive.py
@@ -4,24 +4,46 @@ import socket
 
 def TCP_Socket_Receive():
     while True:
-        #3.接收数据
-        if globalvar.get_value("Receive_Enable")==True:
-            try:
-                globalvar.set_value("sys_log","Tcp socket is receiving...")
+        if globalvar.get_value("TCP_select")=="As TCP Client":
+            #3.接收数据
+            if globalvar.get_value("Receive_Enable")==True:
+                try:
+                    globalvar.set_value("sys_log","Tcp socket is receiving...")
 
-                tcp_socket=globalvar.get_value("tcp_socket")#get_tcp_socket
-                recv_data=tcp_socket.recv(1024)
-                globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
+                    tcp_socket=globalvar.get_value("tcp_socket")#get_tcp_socket
+                    recv_data=tcp_socket.recv(1024)
+                    globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
 
-                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                      globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Receive=True")
-                globalvar.set_value("sys_log", "Tcp socket received...")
+                    print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                          globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Receive=True")
+                    globalvar.set_value("sys_log", "Tcp socket received...")
 
-            #ConnectionAbortedError:[WinError 10053]您的主机中的软件中止了一个已建立的连接。
-            #AttributeError:'str'object has no attribute'decode'
-            #except ConnectionAbortedError:
-            except:
-                print("TCP_Socket_Receive is ConnectionAbortedError...")
-            globalvar.set_value("Receive", False)
+                #ConnectionAbortedError:[WinError 10053]您的主机中的软件中止了一个已建立的连接。
+                #AttributeError:'str'object has no attribute'decode'
+                #except ConnectionAbortedError:
+                except:
+                    print("TCP_Socket_Receive is ConnectionAbortedError...")
+                globalvar.set_value("Receive", False)
+        elif globalvar.get_value("TCP_select") == "As TCP Server":
+            # 3.接收数据
+            if globalvar.get_value("Receive_Enable") == True:
+                try:
+                    globalvar.set_value("sys_log", "Tcp socket is receiving...")
 
+                    tcp_socket = globalvar.get_value("tcp_socket")  # get_tcp_socket
+                    recv_data = tcp_socket.recv(1024)
+                    globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
+
+                    print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                          globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Receive=True")
+                    globalvar.set_value("sys_log", "Tcp socket received...")
+
+                # ConnectionAbortedError:[WinError 10053]您的主机中的软件中止了一个已建立的连接。
+                # AttributeError:'str'object has no attribute'decode'
+                # except ConnectionAbortedError:
+                except:
+                    print("TCP_Socket_Receive is ConnectionAbortedError...")
+                globalvar.set_value("Receive", False)
+        else:
+            pass
         time.sleep(0.1)#0.1s
--- TCP_Socket_tool.py
+++ TCP_Socket_tool.py
@@ -24,76 +24,150 @@ def TCP_Socket_tool():
     #tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #Close之后必须重新创建socket，否则无法重新connect。
     while True:
-        #tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #connect之后不能再次创建socket，否则将自动Close。
+        if globalvar.get_value("TCP_select")=="As TCP Client":
+            #tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #connect之后不能再次创建socket，否则将自动Close。
 
-        #1.创建套接字socket,连接服务器
-        if globalvar.get_value("Connect")==True:
-            globalvar.set_value("sys_log", "Tcp socket is connecting...")
+            #1.创建套接字socket,连接服务器
+            if globalvar.get_value("Connect")==True:
+                globalvar.set_value("sys_log", "Tcp socket is connecting...")
 
-            #解决方式：connect之前tcp_socket.close()。
-            globalvar.set_value("Receive_Enable", False)#Reset_Receive_Enable
-            try:
-                tcp_socket.shutdown(socket.SHUT_RDWR)
-                tcp_socket.close()
-            #UnboundLocalError:local variable'tcp_socket'referenced before assignment
-            #except UnboundLocalError:
-            except:
-                print("TCP_Socket_close is UnboundLocalError...")
+                #解决方式：connect之前tcp_socket.close()。
+                globalvar.set_value("Receive_Enable", False)#Reset_Receive_Enable
+                try:
+                    tcp_socket.shutdown(socket.SHUT_RDWR)
+                    tcp_socket.close()
+                #UnboundLocalError:local variable'tcp_socket'referenced before assignment
+                #except UnboundLocalError:
+                except:
+                    print("TCP_Socket_close is UnboundLocalError...")
 
-            tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            #tcp_socket.bind(("192.168.1.210",1234))
-            globalvar.set_value("tcp_socket",tcp_socket)#set_tcp_socket
-            dest_ip=globalvar.get_value("Server_IP")
-            dest_port=int(globalvar.get_value("Server_PORT"))
-            dest_addr=(dest_ip,dest_port)
-            #在connect之后等待Receive_Message期间，重复connect时报错啦；
-            #ConnectionRefusedError:[WinError 10061]由于目标计算机积极拒绝，无法连接；
-            tcp_socket.connect(dest_addr)
+                tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                #tcp_socket.bind(("192.168.1.210",1234))
+                globalvar.set_value("tcp_socket",tcp_socket)#set_tcp_socket
+                dest_ip=globalvar.get_value("Server_IP")
+                dest_port=int(globalvar.get_value("Server_PORT"))
+                dest_addr=(dest_ip,dest_port)
+                #在connect之后等待Receive_Message期间，重复connect时报错啦；
+                #ConnectionRefusedError:[WinError 10061]由于目标计算机积极拒绝，无法连接；
+                tcp_socket.connect(dest_addr)
 
-            print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Connect=True")
-            globalvar.set_value("sys_log", "Tcp socket connected...")
-            globalvar.set_value("Connect", False)
-            globalvar.set_value("Receive_Enable",True)#Set_Receive_Enable
-        #2.发送数据
-        if globalvar.get_value("Send")==True:
-            globalvar.set_value("sys_log", "Tcp socket is sending...")
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Connect=True")
+                globalvar.set_value("sys_log", "Tcp socket connected...")
+                globalvar.set_value("Connect", False)
+                globalvar.set_value("Receive_Enable",True)#Set_Receive_Enable
+            #2.发送数据
+            if globalvar.get_value("Send")==True:
+                globalvar.set_value("sys_log", "Tcp socket is sending...")
 
-            send_data=globalvar.get_value("send_Message")
-            tcp_socket.send(send_data.encode("utf-8"))
+                send_data=globalvar.get_value("send_Message")
+                tcp_socket.send(send_data.encode("utf-8"))
 
-            print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Send=True")
-            globalvar.set_value("sys_log", "Tcp socket sended...")
-            globalvar.set_value("Send", False)
-        #3.接收数据
-        """
-        if globalvar.get_value("Receive")==True:
-            globalvar.set_value("sys_log", "Tcp socket is receiving...")
-            
-            recv_data = tcp_socket.recv(1024)
-            globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Send=True")
+                globalvar.set_value("sys_log", "Tcp socket sended...")
+                globalvar.set_value("Send", False)
+            #3.接收数据
+            """
+            if globalvar.get_value("Receive")==True:
+                globalvar.set_value("sys_log", "Tcp socket is receiving...")
+                
+                recv_data = tcp_socket.recv(1024)
+                globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
+    
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Receive=True")
+                globalvar.set_value("sys_log", "Tcp socket received...")
+                globalvar.set_value("Receive", False)
+            """
+            #4.关闭套接字socket
+            if globalvar.get_value("Close")==True:
+                globalvar.set_value("Receive_Enable", False)#Reset_Receive_Enable
+                globalvar.set_value("sys_log", "Tcp socket is closing...")
 
-            print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Receive=True")
-            globalvar.set_value("sys_log", "Tcp socket received...")
-            globalvar.set_value("Receive", False)
-        """
-        #4.关闭套接字socket
-        if globalvar.get_value("Close")==True:
-            globalvar.set_value("Receive_Enable", False)#Reset_Receive_Enable
-            globalvar.set_value("sys_log", "Tcp socket is closing...")
+                try:
+                   tcp_socket.shutdown(socket.SHUT_RDWR)
+                   tcp_socket.close()
+                except OSError:
+                   print("TCP_Socket_close is OSError...")
 
-            try:
-               tcp_socket.shutdown(socket.SHUT_RDWR)
-               tcp_socket.close()
-            except OSError:
-               print("TCP_Socket_close is OSError...")
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Close=True")
+                globalvar.set_value("sys_log", "Tcp socket closed...")
+                globalvar.set_value("Close", False)
+        elif globalvar.get_value("TCP_select")=="As TCP Server":
+            # tcp_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            # connect之后不能再次创建socket，否则将自动Close。
 
-            print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                  globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Close=True")
-            globalvar.set_value("sys_log", "Tcp socket closed...")
-            globalvar.set_value("Close", False)
+            # 1.创建套接字socket,连接服务器
+            if globalvar.get_value("Connect") == True:
+                globalvar.set_value("sys_log", "Tcp socket is connecting...")
 
+                # 解决方式：connect之前tcp_socket.close()。
+                globalvar.set_value("Receive_Enable", False)  # Reset_Receive_Enable
+                try:
+                    tcp_socket.shutdown(socket.SHUT_RDWR)
+                    tcp_socket.close()
+                # UnboundLocalError:local variable'tcp_socket'referenced before assignment
+                # except UnboundLocalError:
+                except:
+                    print("TCP_Socket_close is UnboundLocalError...")
+
+                tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # tcp_socket.bind(("192.168.1.210",1234))
+                globalvar.set_value("tcp_socket", tcp_socket)  # set_tcp_socket
+                dest_ip = globalvar.get_value("Server_IP")
+                dest_port = int(globalvar.get_value("Server_PORT"))
+                dest_addr = (dest_ip, dest_port)
+                # 在connect之后等待Receive_Message期间，重复connect时报错啦；
+                # ConnectionRefusedError:[WinError 10061]由于目标计算机积极拒绝，无法连接；
+                tcp_socket.connect(dest_addr)
+
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Connect=True")
+                globalvar.set_value("sys_log", "Tcp socket connected...")
+                globalvar.set_value("Connect", False)
+                globalvar.set_value("Receive_Enable", True)  # Set_Receive_Enable
+            # 2.发送数据
+            if globalvar.get_value("Send") == True:
+                globalvar.set_value("sys_log", "Tcp socket is sending...")
+
+                send_data = globalvar.get_value("send_Message")
+                tcp_socket.send(send_data.encode("utf-8"))
+
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Send=True")
+                globalvar.set_value("sys_log", "Tcp socket sended...")
+                globalvar.set_value("Send", False)
+            # 3.接收数据
+            """
+            if globalvar.get_value("Receive")==True:
+                globalvar.set_value("sys_log", "Tcp socket is receiving...")
+
+                recv_data = tcp_socket.recv(1024)
+                globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
+
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"),globalvar.get_value("Receive_Message"),"Receive=True")
+                globalvar.set_value("sys_log", "Tcp socket received...")
+                globalvar.set_value("Receive", False)
+            """
+            # 4.关闭套接字socket
+            if globalvar.get_value("Close") == True:
+                globalvar.set_value("Receive_Enable", False)  # Reset_Receive_Enable
+                globalvar.set_value("sys_log", "Tcp socket is closing...")
+
+                try:
+                    tcp_socket.shutdown(socket.SHUT_RDWR)
+                    tcp_socket.close()
+                except OSError:
+                    print("TCP_Socket_close is OSError...")
+
+                print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
+                      globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Close=True")
+                globalvar.set_value("sys_log", "Tcp socket closed...")
+                globalvar.set_value("Close", False)
+        else:
+            pass
         time.sleep(0.1)#0.1s
