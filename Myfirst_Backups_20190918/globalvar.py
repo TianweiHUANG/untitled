d8dfbcd548ebb42de78fe71ea531903ff28bbe28
htw01118891 <huangtianwei1988@163.com>
2019-09-18 21:10:45 +0800

    创建文件-201908181549

--- Myfirst_Backups_20190918/globalvar.py
+++ Myfirst_Backups_20190918/globalvar.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 def _init():
     global _global_dict
     _global_dict = {}
@@ -5,6 +8,8 @@ def _init():
 def set_value(name, value):
     _global_dict[name] = value
 
+#参数defValue作为函数get_value()出错时返回的值,如果不指定该参数
+#则默认defValue=None,即函数get_value()出错时返回None。
 def get_value(name, defValue=None):
     try:
         return _global_dict[name]
--- TCP_Socket_Receive.py
+++ TCP_Socket_Receive.py
@@ -1,4 +1,3 @@
-"""
 import time
 import globalvar
 import socket
@@ -6,86 +5,25 @@ import socket
 def TCP_Socket_Receive():
     while True:
         if globalvar.get_value("TCP_select")=="As TCP Client":
-            # 3.接收数据
+            #3.接收数据
             if globalvar.get_value("Receive_Enable") == True:
                 try:
-                    globalvar.set_value("sys_log", "Tcp socket is receiving...")
-
-                    tcp_socket = globalvar.get_value("tcp_socket")  # get_tcp_socket
-                    recv_data = tcp_socket.recv(1024)
-                    globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
-
-                    print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                          globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Receive=True")
-                    globalvar.set_value("sys_log", "Tcp socket received...")
-
-                # ConnectionAbortedError:[WinError 10053]您的主机中的软件中止了一个已建立的连接。
-                # AttributeError:'str'object has no attribute'decode'
-                # except ConnectionAbortedError:
-                except:
-                    print("TCP_Socket_Receive is ConnectionAbortedError...")
-                globalvar.set_value("Receive", False)
-##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
-        elif globalvar.get_value("TCP_select") == "As TCP Server":
-            #myWindow=globalvar.get_value("myWindow")
-            #myWindow.call_MyDialogclass_method("Call MyDialogclass' method successful")
-
-            if globalvar.get_value("Receive_Enable") == True:
-                try:
-                    globalvar.set_value("sys_log", "Tcp socket is receiving...")
-
-                    client_socket = globalvar.get_value("client_socket")
-                    client_addr = globalvar.get_value("client_addr")
-                    recv_data = client_socket.recv(1024)
-                    globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
-
-                    print(client_socket,client_addr,"Receive=True")
-                    globalvar.set_value("sys_log", "Tcp socket received...")
-                except:
-                    print("TCP_Socket_Receive is ConnectionAbortedError...")
-                globalvar.set_value("Receive", False)
-
-        time.sleep(0.1)#0.1s
-"""
-import time
-import globalvar
-import socket
-
-def TCP_Socket_Receive():
-    while True:
-        if globalvar.get_value("TCP_select")=="As TCP Client":
-            # 3.接收数据
-            if globalvar.get_value("Receive_Enable") == True:
-                try:
-                    #globalvar.set_value("sys_log", "Tcp socket is receiving...")
                     myWindow = globalvar.get_value("myWindow")
                     myWindow.printf_sys_log_Func("Tcp socket is receiving...")
 
-                    tcp_socket = globalvar.get_value("tcp_socket")  # get_tcp_socket
+                    tcp_socket = globalvar.get_value("tcp_socket")
                     recv_data = tcp_socket.recv(1024)
                     globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
 
-                    # print(globalvar.get_value("Server_IP"), globalvar.get_value("Server_PORT"),
-                    #       globalvar.get_value("Send_Message"), globalvar.get_value("Receive_Message"), "Receive=True")
-                    #globalvar.set_value("sys_log", "Tcp socket received...")
-                    # myWindow.printf_sys_log_Func(str(client_socket))
-                    # myWindow.printf_sys_log_Func(str(client_addr))
                     myWindow.printf_sys_log_Func("Tcp socket received...")
 
-                # ConnectionAbortedError:[WinError 10053]您的主机中的软件中止了一个已建立的连接。
-                # AttributeError:'str'object has no attribute'decode'
-                # except ConnectionAbortedError:
                 except:
-                    print("TCP_Socket_Receive is ConnectionAbortedError...")
+                    print("TCP_Socket_Receive As TCP Client is Error...")
                 globalvar.set_value("Receive", False)
 ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### #####
         elif globalvar.get_value("TCP_select") == "As TCP Server":
-            #myWindow=globalvar.get_value("myWindow")
-            #myWindow.call_MyDialogclass_method("Call MyDialogclass' method successful")
-
             if globalvar.get_value("Receive_Enable") == True:
                 try:
-                    #globalvar.set_value("sys_log", "Tcp socket is receiving...")
                     myWindow = globalvar.get_value("myWindow")
                     myWindow.printf_sys_log_Func("Tcp socket is receiving...")
 
@@ -94,13 +32,11 @@ def TCP_Socket_Receive():
                     recv_data = client_socket.recv(1024)
                     globalvar.set_value("Receive_Message", recv_data.decode("utf-8"))
 
-                    #print(client_socket,client_addr,"Receive=True")
-                    #globalvar.set_value("sys_log", "Tcp socket received...")
                     myWindow.printf_sys_log_Func(str(client_socket))
                     myWindow.printf_sys_log_Func(str(client_addr))
                     myWindow.printf_sys_log_Func("Tcp socket received...")
                 except:
-                    print("TCP_Socket_Receive is ConnectionAbortedError...")
+                    print("TCP_Socket_Receive As TCP Server is Error...")
                 globalvar.set_value("Receive", False)
 
-        time.sleep(0.1)#0.1s
+        time.sleep(0.1)
