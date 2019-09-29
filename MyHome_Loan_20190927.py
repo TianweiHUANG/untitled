22edae33f733ce1e2ff1616c01938bc0a26e208a
htw01118891 <huangtianwei1988@163.com>
2019-09-29 22:50:50 +0800

    创建文件-201908181549

--- MyHome_Loan_20190927.py
+++ MyHome_Loan_20190927.py
@@ -5,23 +5,41 @@ annualRate = 0.049 # 贷款年利率
 monthRate = annualRate/12 # 贷款月利率
 period = 30 # 贷款期限30年
 
-print("等额本金")#等额本金
+print("等额本金_list")#等额本金_list
 # 还款期数
 month = [n for n in range(1,period*12+1)]
 print("还款期数",month)
 # 每月应还本金
-#monthPrincipalPayment = [loan/(period*12)]*period*12
-monthPrincipalPayment = [loan/(period*12) for n in range(1,period*12+1)]
+#monthPrincipalPayment = [round(loan/(period*12),2)]*period*12
+monthPrincipalPayment = [round(loan/(period*12),2) for n in range(1,period*12+1)]
 print("每月应还本金",monthPrincipalPayment)
 # 每月应还利息
-monthInterestPayment = [(loan - loan*(n-1)/(period*12))*monthRate for n in range(1,period*12+1)]
+monthInterestPayment = [round((loan - loan*(n-1)/(period*12))*monthRate,2) for n in range(1,period*12+1)]
 print("每月应还利息",monthInterestPayment)
 # 每月应还本息
-monthPayment = [(loan - loan*(n-1)/(period*12))*monthRate+loan/(period*12) for n in range(1,period*12+1)]
+monthPayment = [round((loan - loan*(n-1)/(period*12))*monthRate+loan/(period*12),2) for n in range(1,period*12+1)]
 print("每月应还本息",monthPayment)
 
-print("等额本息")#等额本息
+print("等额本息_list")#等额本息_list
+# 还款期数
+month = [n for n in range(1,period*12+1)]
+print("还款期数",month)
 # 每月应还本息
-monthPayment = (loan*monthRate*(1+monthRate)**360)/((1+monthRate)**360-1)
-print("每月应还本息",monthPayment)
-#print("每月应还本息{}".format(round(monthPayment,2)))
+monthPIPayment = round((loan*monthRate*(1+monthRate)**360)/((1+monthRate)**360-1),2)
+monthPIPayment_list=[monthPIPayment for n in range(1,period*12+1)]
+print("每月应还本息",monthPIPayment_list)
+#print("每月应还本息{}".format(round(monthPIPayment,2)))
+
+loanPI = [loan*(1+monthRate)-monthPIPayment]#?
+# 每期应还利息
+monthInterestPayment = [round(loan*monthRate,2)]
+for n in range(1, period*12):
+    loanPI.append((loanPI[n-1]*(1+monthRate)-monthPIPayment))
+    monthInterestPayment.append(round(loanPI[n-1] * monthRate,2))
+    #monthInterestPayment.append(round(loanPI[n-1]*monthRate,2))
+print("每月应还利息",monthInterestPayment)
+
+# 每期应还本金
+#monthPrincipalPayment = [monthPIPayment-monthInterestPayment[n] for n in range(0,len(monthInterestPayment))]
+monthPrincipalPayment = [round(monthPIPayment-monthInterestPayment[n],2) for n in range(0,period*12)]
+print("每月应还本金",monthPrincipalPayment)
