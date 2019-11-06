0b5c1b8f61e2779ed97aa96c6ace749a70bb8a40
htw01118891 <huangtianwei1988@163.com>
2019-11-06 17:27:00 +0800

    创建文件-201908181549

---
+++ Neural_Network_TEST_20191107.py
@@ -0,0 +1,22 @@
+import skimage.data
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy
+
+img = skimage.data.chelsea()
+matplotlib.image.imsave('Neural_Network_img.png', img)
+plt.imshow(img) #Needs to be in row,col order
+plt.savefig("Neural_Network_img_plt.png")
+print(img)
+
+img = skimage.color.rgb2gray(img)
+matplotlib.image.imsave('Neural_Network_img_gray.png', img)
+plt.imshow(img) #Needs to be in row,col order
+plt.savefig("Neural_Network_img_gray_plt.png")
+print(img)
+
+l1_filter = numpy.zeros((2,3,3))
+print(l1_filter)
+l1_filter[0, :, :] = numpy.array([[[-1, 0, 1], [-1, 0, 1],[-1, 0, 1]]])
+l1_filter[1, :, :] = numpy.array([[[1,   1,  1], [0,   0,  0],[-1, -1, -1]]])
+print(l1_filter)
