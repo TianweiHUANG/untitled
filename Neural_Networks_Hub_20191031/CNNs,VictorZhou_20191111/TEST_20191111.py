3d2e7777831651ceef063ba613004506107a8ba6
htw01118891 <huangtianwei1988@163.com>
2019-11-11 11:33:25 +0800

    创建文件-201908181549

--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191111.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191111.py
@@ -1,35 +1,27 @@
-# 01$.@victorczhou-CNN 02$.@victorczhou-MLP 03$.@aliyun-CNN 04$.@白手起家的百万富翁-MLP 05$.@<<Tensorflow 实战>>
-"""
+# 01$.@victorczhou-CNN
+# 02$.@victorczhou-MLP
+# 03$.@aliyun-CNN
+# 04$.@白手起家的百万富翁-MLP
+# 05$.@<<Tensorflow 实战>>
 import mnist
-
-train_images = mnist.train_images()[:1000]
-train_labels = mnist.train_labels()[:1000]
-print(train_images)
-print(type(train_images),train_images.shape)
-print(train_labels)
-print(type(train_labels),train_labels.shape)
-
-test_images = mnist.test_images()[:1000]
-test_labels = mnist.test_labels()[:1000]
-"""
-"""
 import numpy as np
 
-# train_images = np.array([[[1,2,3], [4,5,6], [7,8,9]],[[10,20,30], [40,50,60], [70,80,90]],[[100,200,300], [400,500,600], [700,800,900]]])
-train_images = np.array([[[1,2,3], [4,5,6], [7,8,9]],[[10,20,30], [40,50,60], [70,80,90]]])
-train_labels = np.array([11,12,13])
+# train_images = mnist.train_images()[:1000] # <class 'numpy.ndarray'> (1000, 28, 28)
+# train_labels = mnist.train_labels()[:1000] # <class 'numpy.ndarray'> (1000,)
+train_images = np.array([[[1,2,3,4], [5,6,7,8], [9,10,11,12]],[[10,20,30,40], [50,60,70,80], [90,100,110,120]]])
+train_labels = np.array([1000,2000])
 print(train_images)
 print(type(train_images),train_images.shape)
 print(train_labels)
 print(type(train_labels),train_labels.shape)
-print("-----------------------------------")
 
 for i, (im, label) in enumerate(zip(train_images, train_labels)):
+    print("------ ------ ------")
     print("i:=",type(i),i)
     print("im:=",type(im),im)
     print("label:=",type(label),label)
-"""
 
+"""
 import mnist
 from conv import Conv3x3
 from maxpool import MaxPool2
@@ -60,3 +52,4 @@ print("Softmax(input_len:=13 * 13 * 8, nodes:=10)")
 out = softmax.forward(out)
 print(out)
 print(out.shape)
+"""
