733a0172c90ec60f5d2cfb742bcb0cf5bb48d61d
htw01118891 <huangtianwei1988@163.com>
2019-11-11 15:46:30 +0800

    创建文件-201908181549

--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191111.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191111.py
@@ -3,6 +3,7 @@
 # 03$.@aliyun-CNN
 # 04$.@白手起家的百万富翁-MLP
 # 05$.@<<Tensorflow 实战>>
+"""
 import mnist
 import numpy as np
 
@@ -20,8 +21,8 @@ for i, (im, label) in enumerate(zip(train_images, train_labels)):
     print("i:=",type(i),i)
     print("im:=",type(im),im)
     print("label:=",type(label),label)
-
 """
+
 import mnist
 from conv import Conv3x3
 from maxpool import MaxPool2
@@ -30,26 +31,23 @@ from softmax import Softmax
 conv = Conv3x3(8)                  # 28x28x1 -> 26x26x8
 pool = MaxPool2()                  # 26x26x8 -> 13x13x8
 softmax = Softmax(13 * 13 * 8, 10) # 13x13x8 -> 10
-
-print("###### ###### ###### ###### ###### ###### 01$.train_image[0] ###### ###### ###### ###### ###### ######")
+print("###### ###### ###### ###### ###### ###### 1/4 $.train_image[0] ###### ###### ###### ###### ###### ######")
 image = mnist.train_images()[0]
 image = (image / 255) - 0.5
 print(image)
 print(image.shape)
-
-print("###### ###### ###### ###### ###### ###### 02$.out_conv ###### ###### ###### ###### ###### ######")
+print("###### ###### ###### ###### ###### ###### 2/4 $.out_conv ###### ###### ###### ###### ###### ######")
 print("conv.filters:=",conv.filters)
 print("conv.filters.shape:=",conv.filters.shape)
 out = conv.forward(image)
 print(out)
 print(out.shape)
-print("###### ###### ###### ###### ###### ###### 03$.out_pool ###### ###### ###### ###### ###### ######")
+print("###### ###### ###### ###### ###### ###### 3/4 $.out_pool ###### ###### ###### ###### ###### ######")
 out = pool.forward(out)
 print(out)
 print(out.shape)
-print("###### ###### ###### ###### ###### ###### 04$.out_softmax ###### ###### ###### ###### ###### ######")
+print("###### ###### ###### ###### ###### ###### 4/4 $.out_softmax ###### ###### ###### ###### ###### ######")
 print("Softmax(input_len:=13 * 13 * 8, nodes:=10)")
 out = softmax.forward(out)
 print(out)
 print(out.shape)
-"""
