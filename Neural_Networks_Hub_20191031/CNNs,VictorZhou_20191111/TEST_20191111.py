9e7aa18cc5f827ac1ed2bf4c572a16f1959e180c
htw01118891 <huangtianwei1988@163.com>
2019-11-12 20:27:03 +0800

    创建文件-201908181549

--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191111.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191111.py
@@ -1,8 +1,9 @@
-# 01$.@victorczhou-CNN
-# 02$.@victorczhou-MLP
-# 03$.@aliyun-CNN
-# 04$.@白手起家的百万富翁-MLP
-# 05$.@<<Tensorflow 实战>>
+# 01$.@ victorczhou-CNN
+# 02$.@ victorczhou-MLP
+# 03$.@ aliyun-CNN
+# 04$.@ 白手起家的百万富翁-MLP
+# 05$.@ <<Tensorflow 实战>>
+# 正态分布 /softmax /交叉熵
 """
 import mnist
 import numpy as np
@@ -27,26 +28,26 @@ import mnist
 from conv import Conv3x3
 from maxpool import MaxPool2
 from softmax import Softmax
-print("###### ###### ###### ###### ###### ###### 1/4 $.train_image[0] ###### ###### ###### ###### ###### ######")
+print("######-######-######-######-######-###### 1/4 $.train_image[0] ######-######-######-######-######-######")
 image = mnist.train_images()[0]
 print(image)
 print(image.shape)
 image = (image / 255) - 0.5
 print(image)
 print(image.shape)
-print("###### ###### ###### ###### ###### ###### 2/4 $.out_conv ###### ###### ###### ###### ###### ######")
+print("######-######-######-######-######-###### 2/4 $.out_conv ######-######-######-######-######-######")
 conv = Conv3x3(8)                  # 28x28x1 -> 26x26x8
 print("conv.filters:=",conv.filters)
 print("conv.filters.shape:=",conv.filters.shape)
 out = conv.forward(image)
 print(out)
 print(out.shape)
-print("###### ###### ###### ###### ###### ###### 3/4 $.out_pool ###### ###### ###### ###### ###### ######")
+print("######-######-######-######-######-###### 3/4 $.out_pool ######-######-######-######-######-######")
 pool = MaxPool2()                  # 26x26x8 -> 13x13x8
 out = pool.forward(out)
 print(out)
 print(out.shape)
-print("###### ###### ###### ###### ###### ###### 4/4 $.out_softmax ###### ###### ###### ###### ###### ######")
+print("######-######-######-######-######-###### 4/4 $.out_softmax ######-######-######-######-######-######")
 softmax = Softmax(13 * 13 * 8, 10) # 13x13x8 -> 10
 print("Softmax(input_len:=13 * 13 * 8, nodes:=10)")
 out = softmax.forward(out)
