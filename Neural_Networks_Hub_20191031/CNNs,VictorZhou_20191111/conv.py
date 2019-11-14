fa43755f35367af2efc671bafbb9d8096f570a81
htw01118891 <huangtianwei1988@163.com>
2019-11-14 14:39:37 +0800

    创建文件-201908181549

--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/conv.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/conv.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+'''
+Note: In this implementation, we assume the input is a 2d numpy array for simplicity, because that's
+how our MNIST images are stored. This works for us because we use it as the first layer in our
+network, but most CNNs have many more Conv layers. If we were building a bigger network that needed
+to use Conv3x3 multiple times, we'd have to make the input be a 3d numpy array.
+'''
+
 class Conv3x3:
   # A Convolution layer using 3x3 filters.
 
@@ -13,7 +20,7 @@ class Conv3x3:
   def iterate_regions(self, image):
     '''
     Generates all possible 3x3 image regions using valid padding.
-    - image is a 2d numpy array
+    - image is a 2d numpy array.
     '''
     h, w = image.shape
 
@@ -28,6 +35,8 @@ class Conv3x3:
     Returns a 3d numpy array with dimensions (h, w, num_filters).
     - input is a 2d numpy array
     '''
+    self.last_input = input
+
     h, w = input.shape
     output = np.zeros((h - 2, w - 2, self.num_filters))
 
@@ -35,3 +44,23 @@ class Conv3x3:
       output[i, j] = np.sum(im_region * self.filters, axis=(1, 2))
 
     return output
+
+  def backprop(self, d_L_d_out, learn_rate):
+    '''
+    Performs a backward pass of the conv layer.
+    - d_L_d_out is the loss gradient for this layer's outputs.
+    - learn_rate is a float.
+    '''
+    d_L_d_filters = np.zeros(self.filters.shape)
+
+    for im_region, i, j in self.iterate_regions(self.last_input):
+      for f in range(self.num_filters):
+        d_L_d_filters[f] += d_L_d_out[i, j, f] * im_region
+
+    # Update filters
+    self.filters -= learn_rate * d_L_d_filters
+
+    # We aren't returning anything here since we use Conv3x3 as the first layer in our CNN.
+    # Otherwise, we'd need to return the loss gradient for this layer's inputs, just like every
+    # other layer in our CNN.
+    return None
