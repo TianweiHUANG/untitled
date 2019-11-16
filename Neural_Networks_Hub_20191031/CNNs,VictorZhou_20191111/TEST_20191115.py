b7b9b1e6397b629874aa5aeb25c2d8514b350f3c
htw01118891 <huangtianwei1988@163.com>
2019-11-16 19:12:19 +0800

    创建文件-201908181549

--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191115.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191115.py
@@ -4,18 +4,18 @@
 # 04$.@ 白手起家的百万富翁-MLP
 # 05$.@ <<Tensorflow 实战>>
 # -正态分布 -softmax -交叉熵
-"""
+
 import mnist
 import numpy as np
 
 # train_images = mnist.train_images()[:1000] # <class 'numpy.ndarray'> (1000, 28, 28)
 # train_labels = mnist.train_labels()[:1000] # <class 'numpy.ndarray'> (1000,)
-train_images = np.array([[[1,2,3,4], [5,6,7,8], [9,10,11,12]],[[10,20,30,40], [50,60,70,80], [90,100,110,120]]])
-train_labels = np.array([1000,2000])
-print(train_images)
-print(type(train_images),train_images.shape)
-print(train_labels)
-print(type(train_labels),train_labels.shape)
+# train_images = np.array([[[1,2,3,4], [5,6,7,8], [9,10,11,12]],[[10,20,30,40], [50,60,70,80], [90,100,110,120]]])
+# train_labels = np.array([1000,2000])
+# print(train_images)
+# print(type(train_images),train_images.shape)
+# print(train_labels)
+# print(type(train_labels),train_labels.shape)
 
 # for i, (im, label) in enumerate(zip(train_images, train_labels)):
 #     print("------ ------ ------")
@@ -31,32 +31,27 @@ from maxpool import MaxPool2
 from softmax import Softmax
 print("######-######-######-######-######-###### 1/5 $.train_image[0] ######-######-######-######-######-######")
 image = mnist.train_images()[0]
-# print(image)
-print(image.shape)
+print(image.shape,"\n",image)
 image = (image / 255) - 0.5
-# print(image)
-print(image.shape)
+print(image.shape,"\n",image)
 print("######-######-######-######-######-###### 2/5 $.out_conv ######-######-######-######-######-######")
 conv = Conv3x3(8)                  # 28x28x1 -> 26x26x8
-# print("conv.filters:=",conv.filters)
-print("conv.filters.shape:=",conv.filters.shape)
+print("filters.shape:=",conv.filters.shape,"\n","filters:=",conv.filters)
 out = conv.forward(image)
-# print(out)
-print(out.shape)
+print(out.shape,"\n",out)
 print("######-######-######-######-######-###### 3/5 $.out_pool ######-######-######-######-######-######")
 pool = MaxPool2()                  # 26x26x8 -> 13x13x8
 out = pool.forward(out)
-# print(out)
-print(out.shape)
+print(out.shape,"\n",out)
 print("######-######-######-######-######-###### 4/5 $.out_softmax ######-######-######-######-######-######")
 softmax = Softmax(13 * 13 * 8, 10) # 13x13x8 -> 10
-print("Softmax(input_len:=13 * 13 * 8, nodes:=10)")
+print("softmax(input_len:=13 * 13 * 8, nodes:=10)")
 out = softmax.forward(out)
-print(out)
-print(out.shape)
+print(out.shape,"\n",out)
 print("######-######-######-######-######-###### 5/5 $.loss,acc ######-######-######-######-######-######")
-label = mnist.train_labels()[0]
 # Calculate cross-entropy loss and accuracy. np.log() is the natural log.
+label = mnist.train_labels()[0]
 loss = -np.log(out[label])
 acc = 1 if np.argmax(out) == label else 0
 print("label:=",label,"label.shape:=",label.shape,"acc:=",acc,"loss:=",loss)
+"""
--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/cnn.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/cnn.py
@@ -1,19 +1,19 @@
-import mnist # ok
-import numpy as np # ok
-from conv import Conv3x3 # ok
-from maxpool import MaxPool2 # ok
-from softmax import Softmax # ok
+import mnist
+import numpy as np
+from conv import Conv3x3
+from maxpool import MaxPool2
+from softmax import Softmax
 
-# We only use the first 1k examples of each set in the interest of time. # ok
-# Feel free to change this if you want. # ok
-train_images = mnist.train_images()[:1000] # ok
-train_labels = mnist.train_labels()[:1000] # ok
-test_images = mnist.test_images()[:1000] # ok
-test_labels = mnist.test_labels()[:1000] # ok
+# We only use the first 1k examples of each set in the interest of time.
+# Feel free to change this if you want.
+train_images = mnist.train_images()[:1000]
+train_labels = mnist.train_labels()[:1000]
+test_images = mnist.test_images()[:1000]
+test_labels = mnist.test_labels()[:1000]
 
-conv = Conv3x3(8) # ok                  # 28x28x1 -> 26x26x8
-pool = MaxPool2() # ok                  # 26x26x8 -> 13x13x8
-softmax = Softmax(13 * 13 * 8, 10) # ok # 13x13x8 -> 10
+conv = Conv3x3(8)                  # 28x28x1 -> 26x26x8
+pool = MaxPool2()                  # 26x26x8 -> 13x13x8
+softmax = Softmax(13 * 13 * 8, 10) # 13x13x8 -> 10
 
 def forward(image, label):
   '''
@@ -56,21 +56,22 @@ def train(im, label, lr=.005):
 
   return loss, acc
 
-print('MNIST CNN initialized!') # ok
+print('MNIST CNN initialized!')
 
-# Train the CNN for 3 epochs # ok
-for epoch in range(3): # ok
-  print('--- Epoch %d ---' % (epoch + 1)) # ok
+# Train the CNN for 3 epochs
+for epoch in range(3):
+  print('--- Epoch %d ---' % (epoch + 1))
 
-  # Shuffle the training data # ok
-  permutation = np.random.permutation(len(train_images)) # ok
-  train_images = train_images[permutation] # ok
-  train_labels = train_labels[permutation] # ok
+  # Shuffle the training data
+  permutation = np.random.permutation(len(train_images))
+  train_images = train_images[permutation]
+  train_labels = train_labels[permutation]
 
-  # Train! # ok
-  loss = 0 # ok
-  num_correct = 0 # ok
-  for i, (im, label) in enumerate(zip(train_images, train_labels)): # ? - 2
+  # Train!
+  loss = 0
+  num_correct = 0
+  # Ng # Ng # Ng # Ng # Ng # Ng
+  for i, (im, label) in enumerate(zip(train_images, train_labels)):
     if i % 100 == 99:
       print(
         '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
@@ -82,16 +83,17 @@ for epoch in range(3): # ok
     l, acc = train(im, label)
     loss += l
     num_correct += acc
+  # Ng # Ng # Ng # Ng # Ng # Ng
 
-# Test the CNN # ok
-print('\n--- Testing the CNN ---') # ok
-loss = 0 # ok
-num_correct = 0 # ok
-for im, label in zip(test_images, test_labels): # ok
-  _, l, acc = forward(im, label) # ? - 1
-  loss += l # ok
-  num_correct += acc # ok
+# Test the CNN
+print('\n--- Testing the CNN ---')
+loss = 0
+num_correct = 0
+for im, label in zip(test_images, test_labels):
+  _, l, acc = forward(im, label) # Ng # Ng # Ng # Ng # Ng # Ng
+  loss += l
+  num_correct += acc
 
-num_tests = len(test_images) # ok
-print('Test Loss:', loss / num_tests) # ok
-print('Test Accuracy:', num_correct / num_tests) # ok
+num_tests = len(test_images)
+print('Test Loss:', loss / num_tests)
+print('Test Accuracy:', num_correct / num_tests)
