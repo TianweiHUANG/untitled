4771cf4144efa68ec90e7b677c44743b609128fa
htw01118891 <huangtianwei1988@163.com>
2019-11-20 11:01:08 +0800

    创建文件-201908181549

--- Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191118.py
+++ Neural_Networks_Hub_20191031/CNNs,VictorZhou_20191111/TEST_20191118.py
@@ -8,20 +8,20 @@
 import mnist
 import numpy as np
 
-train_images = mnist.train_images()[:1000]# <class 'numpy.ndarray'> (1000, 28, 28)
-train_labels = mnist.train_labels()[:1000]# <class 'numpy.ndarray'> (1000,)
-# train_images = np.array([[[1,2,3,4], [5,6,7,8], [9,10,11,12]],[[10,20,30,40], [50,60,70,80], [90,100,110,120]]])
-# train_labels = np.array([1000,2000])
+# train_images = mnist.train_images()[:1000]# <class 'numpy.ndarray'> (1000, 28, 28)
+# train_labels = mnist.train_labels()[:1000]# <class 'numpy.ndarray'> (1000,)
+train_images = np.array([[[1,2,3,4], [5,6,7,8], [9,10,11,12]],[[10,20,30,40], [50,60,70,80], [90,100,110,120]]])
+train_labels = np.array([1000,2000])
 print(train_images)
 print(type(train_images),train_images.shape)
 print(train_labels)
 print(type(train_labels),train_labels.shape)
 
-# for i, (im, label) in enumerate(zip(train_images, train_labels)):
-#     print("------ ------ ------")
-#     print("i:=",type(i),i)
-#     print("im:=",type(im),im)
-#     print("label:=",type(label),label)
+for i, (im, label) in enumerate(zip(train_images, train_labels)):
+    print("------ ------ ------")
+    print("i:=",type(i),i)
+    print("im:=",type(im),im)
+    print("label:=",type(label),label)
 
 """
 import mnist
@@ -49,8 +49,8 @@ print("softmax(input_len:=13 * 13 * 8, nodes:=10)")
 out = softmax.forward(out)
 print(out.shape,"\n",out)
 print("######-######-######-######-######-###### 5/5 $.loss,acc ######-######-######-######-######-######")
-# Calculate cross-entropy loss and accuracy. np.log() is the natural log.
 label = mnist.train_labels()[0]
+# Calculate cross-entropy loss and accuracy. np.log() is the natural log.
 loss = -np.log(out[label])
 acc = 1 if np.argmax(out) == label else 0
 print("label:=",label,"label.shape:=",label.shape,"acc:=",acc,"loss:=",loss)
